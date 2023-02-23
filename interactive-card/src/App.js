abd3b61e0cfd86bcfffa6f5759b81411ad186214
Caio Bukvar <caiobukvar@gmail.com>
2023-02-23 18:48:31 -0300

    fixes layout

--- interactive-card/src/App.js
+++ interactive-card/src/App.js
@@ -9,9 +9,10 @@ import {
   Button,
   HStack,
   Text,
-  Image
+  Image,
+  Stack
 } from '@chakra-ui/react'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import bgCardFront from './assets/images/bg-card-front.png';
 import bgCardBack from './assets/images/bg-card-back.png';
 import Complete from './assets/images/icon-complete.svg';
@@ -46,7 +47,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <VStack className="card-container">
+        <Stack className="card-container">
           <VStack className='card-top'>
             <VStack className='bgcard card--1' src={bgCardFront} alt="cc-front">
               <Text className='card-number-text'>{input.cardNumber || '0000 0000 0000 0000'}</Text>
@@ -54,80 +55,84 @@ function App() {
               <Text className='card-exp-text'>{`${input.expDateMM || "00"}/${input.expDateYY || "00"}`}</Text>
             </VStack>
             <VStack className='bgcard card--2' src={bgCardBack} alt="cc-back">
-              <Text className='bgtext'>{input.CVC || '000'}</Text>
+              <Text className='cvc-text'>{input.CVC || '000'}</Text>
             </VStack>
           </VStack>
           {!submited ? <VStack className='form-container'>
-            <FormControl isInvalid={isErrorCardholder} >
-              <FormLabel>CARDHOLDER NAME</FormLabel>
-              <Input
-                type='text'
-                value={input.cardholder}
-                placeholder="e.g. Jane Appleseed"
-                onChange={(e) => setInput({ ...input, cardholder: e.target.value })}
-              />
-              {isErrorCardholder && (
-                <FormErrorMessage>Cardholder name is required.</FormErrorMessage>
-              )}
-            </FormControl>
-
-            <FormControl isInvalid={isErrorCardNumber} >
-              <FormLabel>CARD NUMBER</FormLabel>
-              <Input
-                type='text'
-                value={input.cardNumber}
-                placeholder="e.g. 1234 5678 9123 0000"
-                onChange={(e) => setInput({ ...input, cardNumber: e.target.value })}
-              />
-              {isErrorCardNumber && (
-                <FormErrorMessage>Card number is required.</FormErrorMessage>
-              )}
-            </FormControl>
-
-            <HStack textAlign='start' alignItems='start' spacing={3}>
-              <FormControl isInvalid={isErrorExpDate} >
-                <FormLabel>EXP. DATE (MM/YY)</FormLabel>
-                <HStack>
-                  <Input
-                    type='number'
-                    value={input.expDateMM}
-                    placeholder="MM"
-                    onChange={(e) => setInput({ ...input, expDateMM: e.target.value })}
-                  />
-                  <Input
-                    type='number'
-                    value={input.expDateYY}
-                    placeholder="YY"
-                    onChange={(e) => setInput({ ...input, expDateYY: e.target.value })}
-                  />
-                </HStack>
-                {isErrorExpDate && (
-                  <FormErrorMessage>Card expiration date is required.</FormErrorMessage>
-                )}
-              </FormControl>
-
-              <FormControl isInvalid={isErrorCVC} >
-                <FormLabel>CVC</FormLabel>
+            <VStack className='form-content'>
+              <FormControl isInvalid={isErrorCardholder} >
+                <FormLabel>CARDHOLDER NAME</FormLabel>
                 <Input
                   type='text'
-                  value={input.CVC}
-                  placeholder="e.g. 123"
-                  onChange={(e) => setInput({ ...input, CVC: e.target.value })}
+                  value={input.cardholder}
+                  placeholder="e.g. Jane Appleseed"
+                  onChange={(e) => setInput({ ...input, cardholder: e.target.value })}
                 />
-                {isErrorCVC && (
-                  <FormErrorMessage>CVC is required.</FormErrorMessage>
+                {isErrorCardholder && (
+                  <FormErrorMessage>Cardholder name is required.</FormErrorMessage>
                 )}
               </FormControl>
-            </HStack>
-            <Button
-              w='100%'
-              bgColor='#21092F'
-              color='#fff'
-              fontWeight='500'
-              onClick={() => sendData('open')}
-            >
-              Confirm
-            </Button>
+
+              <FormControl isInvalid={isErrorCardNumber} >
+                <FormLabel>CARD NUMBER</FormLabel>
+                <Input
+                  type='text'
+                  value={input.cardNumber}
+                  placeholder="e.g. 1234 5678 9123 0000"
+                  onChange={(e) => setInput({ ...input, cardNumber: e.target.value })}
+                />
+                {isErrorCardNumber && (
+                  <FormErrorMessage>Card number is required.</FormErrorMessage>
+                )}
+              </FormControl>
+
+              <HStack textAlign='start'>
+                <FormControl isInvalid={isErrorExpDate} >
+                  <FormLabel>EXP. DATE (MM/YY)</FormLabel>
+                  <HStack>
+                    <Input
+                      type='number'
+                      value={input.expDateMM}
+                      placeholder="MM"
+                      onChange={(e) => setInput({ ...input, expDateMM: e.target.value })}
+                    />
+                    <Input
+                      type='number'
+                      value={input.expDateYY}
+                      placeholder="YY"
+                      onChange={(e) => setInput({ ...input, expDateYY: e.target.value })}
+                    />
+                  </HStack>
+                  {isErrorExpDate && (
+                    <FormErrorMessage h='30px'>Card expiration date is required.</FormErrorMessage>
+                  )}
+                </FormControl>
+
+                <FormControl isInvalid={isErrorCVC} >
+                  <FormLabel>CVC</FormLabel>
+                  <HStack alingItems='center'>
+                    <Input
+                      type='text'
+                      value={input.CVC}
+                      placeholder="e.g. 123"
+                      onChange={(e) => setInput({ ...input, CVC: e.target.value })}
+                    />
+                  </HStack>
+                  {isErrorCVC && (
+                    <FormErrorMessage h='30px' textAlign='start'>CVC is required.</FormErrorMessage>
+                  )}
+                </FormControl>
+              </HStack>
+              <Button
+                w='100%'
+                bgColor='#21092F'
+                color='#fff'
+                fontWeight='500'
+                onClick={() => sendData('open')}
+              >
+                Confirm
+              </Button>
+            </VStack>
           </VStack> :
             <VStack spacing={10}>
               <VStack spacing={5}>
@@ -147,7 +152,7 @@ function App() {
             </VStack>
           }
 
-        </VStack>
+        </Stack>
       </header>
     </div>
   );
